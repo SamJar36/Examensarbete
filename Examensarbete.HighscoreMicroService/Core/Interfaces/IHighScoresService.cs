47063d9936fc1050cb70f82c6b920bd3de965664
SamJar36 <samandreasjarva@gmail.com>
2026-01-13 15:35:22 +0100

    -IHighScoresService -HighScoresService, empty at the moment -Shared, AddScoreRequest, PlayerResponse -Swagger support -Dependency Injection for HighScoresService into HighScoresController

---
+++ Examensarbete.HighscoreMicroService/Core/Interfaces/IHighScoresService.cs
@@ -0,0 +1,15 @@
+﻿using Examensarbete.HighscoreMicroService.Shared.Requests;
+using Examensarbete.HighscoreMicroService.Shared.Responses;
+
+namespace Examensarbete.HighscoreMicroService.Core.Interfaces
+{
+    public interface IHighScoresService
+    {
+        Task<bool> DeleteHighscoreEntryAsync(int id);
+        Task DeleteHighscoresAsync();
+        Task<PlayerResponse> GetByIdAsync(int id);
+        Task<List<PlayerResponse>> GetHighScoresAsync();
+        Task<bool> ResetHighScoresAsync();
+        Task<PlayerResponse> SubmitScoreAsync(AddScoreRequest request);
+    }
+}
---
+++ Examensarbete.HighscoreMicroService/Core/Services/HighScoresService.cs
@@ -0,0 +1,48 @@
+﻿using Examensarbete.HighscoreMicroService.Shared.Responses;
+using Examensarbete.HighscoreMicroService.Shared.Requests;
+using Examensarbete.HighscoreMicroService.Core.Interfaces;
+
+namespace Examensarbete.HighscoreMicroService.Core.Services;
+
+public class HighScoresService : IHighScoresService
+{
+    public async Task<List<PlayerResponse>> GetHighScoresAsync()
+    {
+        return new List<PlayerResponse>();
+    }
+
+    public async Task<PlayerResponse> SubmitScoreAsync(AddScoreRequest request)
+    {
+        return new PlayerResponse();
+    }
+
+    public async Task<bool> ResetHighScoresAsync()
+    {
+        DeleteAllScores();
+        //then populate list with preset scores
+        //if successful, return true
+        bool result = true;
+        return result;
+    }
+
+    public async Task<bool> DeleteHighscoreEntryAsync(int id)
+    {
+        bool result = true;
+        return result;
+    }
+
+    public async Task DeleteHighscoresAsync()
+    {
+        DeleteAllScores();
+    }
+
+    public async Task<PlayerResponse> GetByIdAsync(int id)
+    {
+        return new PlayerResponse();
+    }
+
+    private void DeleteAllScores()
+    {
+        // do the thing
+    }
+}
