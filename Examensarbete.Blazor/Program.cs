3743493828719d983c603fa618c1006d93b64078
SamJar36 <samandreasjarva@gmail.com>
2026-01-21 21:06:34 +0100

    fixed docker compose, endpoints not working yet

--- Examensarbete.Blazor/Program.cs
+++ Examensarbete.Blazor/Program.cs
@@ -6,6 +6,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddHttpClient("ApiGateway", client =>
+{
+    client.BaseAddress = new Uri("http://apigateway:8080/");
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
