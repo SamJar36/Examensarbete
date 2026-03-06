a04ef2de30a34325409a161ec0f2c1bffe7bdd05
SamJar36 <samandreasjarva@gmail.com>
2026-03-06 22:42:14 +0100

    Player/PlayerInventory controllers, added endpoints structure

--- Examensarbete.PlayerMicroService/API/Controllers/PlayerController.cs
+++ Examensarbete.PlayerMicroService/API/Controllers/PlayerController.cs
@@ -6,6 +6,40 @@ namespace Examensarbete.PlayerMicroService.Controllers
     [Route("[controller]")]
     public class PlayerController : ControllerBase
     {
-        
+        [HttpGet]
+        public async Task<IActionResult> GetPlayersAsync()
+        {
+           return Ok(); 
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPlayerByIdAsync(Guid id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePlayerAsync([FromBody] object request)
+        {
+            return Created("", null);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdatePlayerAsync(Guid id, [FromBody] object request)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePlayerAsync(Guid id)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete("reset")]
+        public async Task<IActionResult> ResetPlayersAsync()
+        {
+            return NoContent();
+        }
     }
 }
