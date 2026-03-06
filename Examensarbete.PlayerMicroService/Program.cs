f49f774758a49bd5471d1a6c53de0d81df8b3586
SamJar36 <samandreasjarva@gmail.com>
2026-03-06 21:36:48 +0100

    added swagger

--- Examensarbete.PlayerMicroService/Program.cs
+++ Examensarbete.PlayerMicroService/Program.cs
@@ -1,20 +1,29 @@
+using Microsoft.AspNetCore.Builder;
+// using Examensarbete.HighscoreMicroService.Core.Interfaces;
+// using Examensarbete.HighscoreMicroService.Core.Services;
+// using Examensarbete.HighscoreMicroService.Data.Interfaces;
+// using Examensarbete.HighscoreMicroService.Data.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// builder.Services.AddScoped<IHighScoresService, HighScoresService>();
+// builder.Services.AddScoped<IHighScoresRepository, HighScoresRepository>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
