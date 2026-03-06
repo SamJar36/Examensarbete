4bfa6031166f5c59d0c08f79ecaa430ae0d673ce
SamJar36 <samandreasjarva@gmail.com>
2026-03-06 23:21:58 +0100

    added basic structure inside PlayerInventoryService, IPlayerInventoryService

--- Examensarbete.PlayerMicroService/Core/Interfaces/IPlayerInventoryService.cs
+++ Examensarbete.PlayerMicroService/Core/Interfaces/IPlayerInventoryService.cs
@@ -1,5 +1,15 @@
+using Examensarbete.PlayerMicroService.Shared.Responses;
+using Examensarbete.PlayerMicroService.Shared.Requests;
+
 namespace Examensarbete.PlayerMicroService.Core.Interfaces;
 public interface IPlayerInventoryService
 {
-    
+    Task<List<PlayerInventoryResponse>> GetPlayerInventoryAsync(Guid playerId);
+    Task<PlayerInventoryResponse> GetInventoryAsync(Guid playerId);
+    Task<PlayerInventoryResponse> CreateInventoryAsync(Guid playerId, CreatePlayerInventoryRequest request);
+    Task<PlayerInventoryResponse> UpdateInventoryAsync(Guid playerId, UpdatePlauerInventoryRequest request);
+    Task<bool> ResetInventoryAsync(Guid playerId);
+    Task<bool> DeleteInventoryItemAsync(Guid playerId);
+    Task<bool> UpdateCoinsAsync(Guid playerId, int coins);
+    Task<bool> ResetCoinsAsync(Guid playerId);
 }
--- Examensarbete.PlayerMicroService/Core/Interfaces/IPlayerService.cs
+++ Examensarbete.PlayerMicroService/Core/Interfaces/IPlayerService.cs
@@ -1,6 +1,7 @@
 using Examensarbete.PlayerMicroService.Shared.Requests;
 using Examensarbete.PlayerMicroService.Shared.Responses;
 
+namespace Examensarbete.PlayerMicroService.Core.Interfaces;
 public interface IPlayerService
 {
     Task<List<PlayerResponse>> GetAllPlayersAsync();
