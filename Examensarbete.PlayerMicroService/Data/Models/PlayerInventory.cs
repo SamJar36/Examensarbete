dd714e9e3e156c2a04c44c88461f30d4ffe76083
SamJar36 <samandreasjarva@gmail.com>
2026-03-07 00:41:28 +0100

    added models, inventory enums

--- Examensarbete.PlayerMicroService/Data/Models/PlayerInventory.cs
+++ Examensarbete.PlayerMicroService/Data/Models/PlayerInventory.cs
@@ -1,5 +1,19 @@
+using Examensarbete.PlayerMicroService.Shared.Enums;
+
 namespace Examensarbete.PlayerMicroService.Data.Models;
+
 public class PlayerInventory
 {
-
+    public Guid PlayerId { get; set; }
+    public int MaxHearts { get; set; }
+    public int CurrentHearts { get; set; }
+    public int ExtraLives { get; set; }
+    public int Coins { get; set; }
+    public int Keys { get; set; }
+    public bool HasBigKey { get; set; }
+    public List<ConsumableItem> ConsumableItems { get; set; } = new List<ConsumableItem>();
+    public List<Item> Items { get; set; } = new List<Item>();
+    public List<Egg> Eggs { get; set; } = new List<Egg>();
+    public List<FoodYummies> FoodYummies { get; set; } = new List<FoodYummies>();
+    public List<Curses> Curses { get; set; } = new List<Curses>();
 }
