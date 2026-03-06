a00b4fcbc6237f0dd84128ab1b127f434301af29
SamJar36 <samandreasjarva@gmail.com>
2026-03-06 22:42:14 +0100

    Player/PlayerInventory controllers, added endpoints structure

--- Examensarbete.PlayerMicroService/API/Controllers/PlayerInventoryController.cs
+++ Examensarbete.PlayerMicroService/API/Controllers/PlayerInventoryController.cs
@@ -6,6 +6,58 @@ namespace Examensarbete.PlayerMicroService.Controllers
     [Route("[controller]")]
     public class PlayerInventoryController : ControllerBase
     {
-        
+        [HttpGet]
+        public async Task<IActionResult> GetInventoriesAsync()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{playerId}")]
+        public async Task<IActionResult> GetInventoryAsync(Guid playerId)
+        {
+            return Ok();
+        }
+
+        [HttpPost("{playerId}")]
+        public async Task<IActionResult> CreateInventoryAsync(Guid playerId, [FromBody] object request)
+        {
+            return Created("", null);
+        }
+
+        [HttpPut("{playerId}")]
+        public async Task<IActionResult> UpdateInventoryAsync(Guid playerId, [FromBody] object request)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete("{playerId}/reset")]
+        public async Task<IActionResult> ResetInventoryAsync(Guid playerId)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete("{playerId}/item/{itemName}")]
+        public async Task<IActionResult> DeleteInventoryItemAsync(Guid playerId, string itemName)
+        {
+            return NoContent();
+        }
+
+        [HttpPut("{playerId}/coins/{amount}")]
+        public async Task<IActionResult> UpdateCoinsAsync(Guid playerId, int amount)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete("{playerId}/coins/reset")]
+        public async Task<IActionResult> ResetCoinsAsync(Guid playerId)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete("{playerId}/eggs/reset")]
+        public async Task<IActionResult> ResetEggsAsync(Guid playerId)
+        {
+            return NoContent();
+        }
     }
 }
